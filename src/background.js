fb3e43e510bfc36a429025842f77705d97c94996
xVemu <38427679+xVemu@users.noreply.github.com>
2022-01-16 15:12:48 +0100

    * updated to manifest v3 * applied new eslint rules

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
-`use stict`;
+'use strict'
 
 chrome.tabs.onUpdated.addListener((_tabId, _changeInfo, tab) => {
-    if (tab.url.match(new RegExp(`(\\?|&)(gclid|fbclid)=(.*?&|.*)`, `g`)))
+    if (tab.url.match(new RegExp('[?&](gclid|fbclid)=(.*?&|.*)', 'g')))
         chrome.tabs.update(tab.id,
-            { url: tab.url.replace(new RegExp(`(gclid|fbclid)=(.*?&|.*)`, `g`), '') });
-});
+            { url: tab.url.replace(new RegExp('(gclid|fbclid)=(.*?&|.*)', 'g'), '') })
+})
